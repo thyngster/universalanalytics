0859c25fdce0d7470958d09796c6643039ab8c32
thyngster <thyngster@gmail.com>
2013-11-16 00:06:11 +0100

    Update disqus_tracking.js

--- disqus_tracking.js
+++ disqus_tracking.js
@@ -1,12 +1,26 @@
-// Disqus is loaded asynchronously so let's wait till window has been fully loaded.
-// If using ga.js, just change the ga() call to _gaq.push(['trackEvent','Disqus',{ACTION}, windows.location.href]);
+
+/**
+* disqus_tracking.js v1.0.0 - 2013-11-16
+* https://github.com/thyngster/forms_cross_domains.js
+* (c) 2013 David Vallejo ( @thyng )
+* license: www.opensource.org/licenses/mit-license.php
+*
+* 
+* If using ga.js, just change the ga() call to _gaq.push(['trackEvent','Disqus',{ACTION}, windows.location.href]);
+* Disqus is loaded asynchronously and callbacks configuration needs to be set before Disqus si loaded so we basically 
+* what we're doing is reseting it with the new callback values on winwdow.load event ( cross-browser compatibility ).
+* Modify Event category, action and label to fit your needs.
+*
+*/
 
 window.onload = function(){
-    DISQUS.reset({
-          reload: true,
-          config: function () {
-          this.callbacks.onNewComment = [function(){ ga('send', 'event', 'Disqus', 'New Comment', window.location.href); }];
-          this.callbacks.onPaginate = [function(e){ ga('send', 'event', 'Disqus', 'Pagination', window.location.href); }];
-          }
-     });
+    if(window.DISQUS){
+        DISQUS.reset({
+              reload: true,
+              config: function () {
+              this.callbacks.onNewComment = [function(){ ga('send', 'event', 'Disqus', 'New Comment', window.location.href); }];
+              this.callbacks.onPaginate = [function(e){ ga('send', 'event', 'Disqus', 'Pagination', window.location.href); }];
+              }
+         });
+    }
 };
